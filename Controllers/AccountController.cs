0765a32633b8727834181b7c8c9ce94a9d32467f
Hugo Novais <a96267@alunos.uminho.pt>
2023-02-19 16:07:50 +0000

    Add project files.

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+public class AccountController : Controller
+{
+    [HttpGet]
+    public ActionResult Register()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public ActionResult Register(RegisterViewModel model)
+    {
+        if (ModelState.IsValid)
+        {
+            // Create a new user using the model data
+            var user = new User
+            {
+                Name = model.Name,
+                Username = model.Username,
+                Email = model.Email,
+                Password = model.Password
+            };
+
+            // Add the user to the Users DbSet and save changes to the database
+            using (var db = new MyDbContext())
+            {
+                db.User.Add(user);
+                db.SaveChanges();
+            }
+
+            // Redirect to the appropriate page after registration
+            return RedirectToAction("Index", "Home");
+        }
+
+        return View(model);
+    }
+}
---
+++ ORM/MyDbContext.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MySql.Data.MySqlClient;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+public class MyDbContext : DbContext
+{
+    public DbSet<User> User { get; set; }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        string connectionString = "server=localhost;port=3306;user=root;database=unnamed;password=8426";
+        optionsBuilder.UseMySQL(connectionString);
+    }
+}
+public class User
+{
+    [Key]
+    [Column("userID")]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+
+    [Required]
+    [Column("name")]
+    [StringLength(50)]
+    public string Name { get; set; }
+
+    [Required]
+    [Column("username")]
+    [StringLength(50)]
+    public string Username { get; set; }
+
+    [Required]
+    [Column("userEmail")]
+    [StringLength(50)]
+    public string Email { get; set; }
+
+    [Required]
+    [Column("userPassword")]
+    [StringLength(50)]
+    public string Password { get; set; }
+}
