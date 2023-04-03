e3dd017a69d4d01d32ac807950998e8f7243fec4
Hugo Novais <a96267@alunos.uminho.pt>
2023-04-03 14:59:23 +0100

    Shopping Page - Product Filters Working I think

--- Controllers/ShoppingController.cs
+++ Controllers/ShoppingController.cs
@@ -1,28 +1,120 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Query.SqlExpressions;
 using Org.BouncyCastle.Crypto;
 using System.Linq;
 
 public class ShoppingController : Controller
 {
     [HttpGet]
-    public IActionResult Shopping()
+    [Route("shopping")]
+    public IActionResult Shopping(ShoppingViewModel m)
     {
         using (var db = new MyDbContext())
         {
-            var products = db.Product.ToList();
+            if (m.DistrictCounties == null)
+            {
+                var products = db.Product.ToList();
+                var stores = db.Store.ToList();
+                var districtCounties = GetDistrictCounties();
+
+                var model = new ShoppingViewModel
+                {
+                    Min = 0,
+                    Max = 1000,
+                    Products = products,
+                    Stores = stores,
+                    DistrictCounties = districtCounties
+                };
+
+                return View(model);
+            }
+
+            else 
+            {
+                var products = m.Products;
+
+                if (!string.IsNullOrEmpty(m.Category))
+                {
+                    products = db.Product.Where(p => p.Category == m.Category).ToList();
+                }
+
+                if (m.Min.HasValue)
+                {
+                    products = products.Where(p => p.Price >= m.Min.Value).ToList();
+                }
+
+                if (m.Max.HasValue)
+                {
+                    products = products.Where(p => p.Price <= m.Max.Value).ToList();
+                }
+
+                var stores = db.Store.ToList();
+                var districtCounties = GetDistrictCounties();
+
+                var model = new ShoppingViewModel
+                {
+                    Min = m.Min,
+                    Max = m.Max,
+                    Products = products.ToList(),
+                    Stores = stores,
+                    DistrictCounties = districtCounties,
+                    Category = m.Category,
+                    District = m.District,
+                    County = m.County
+                };
+
+                return View(model);
+            }
+        }
+    }
+    
+
+    [HttpPost]
+    public IActionResult Filter(ShoppingViewModel model)
+    {
+        using (var db = new MyDbContext())
+        {
+            var products = db.Product.AsQueryable();
             var stores = db.Store.ToList();
             var districtCounties = GetDistrictCounties();
 
-            var model = new ShoppingViewModel
+            if (!string.IsNullOrEmpty(model.Category))
             {
-                Min = 0,
-                Max = 1000,
-                Products = products,
-                Stores = stores,
-                DistrictCounties = districtCounties
+                products = products.Where(p => p.Category == model.Category);
+            }
+
+            if (model.Min.HasValue)
+            {
+                products = products.Where(p => p.Price >= model.Min.Value);
+            }
+
+            if (model.Max.HasValue)
+            {
+                products = products.Where(p => p.Price <= model.Max.Value);
+            }
+
+            var filteredProducts = products.ToList();
+
+            if (filteredProducts.Count == 0)
+            {
+                ViewBag.Message = "No products were found matching your search criteria.";
+            }
+
+            var filteredStores = stores.Where(s => filteredProducts.Any(p => p.Store == s.Id)).ToList();
+
+            var resultModel = new ShoppingViewModel
+            {
+                Min = model.Min,
+                Max = model.Max,
+                Products = filteredProducts,
+                Stores = filteredStores,
+                DistrictCounties = districtCounties,
+                District = model.District, // include selected district in the view model
+                County = model.County // include selected county in the view model
             };
 
-            return View(model);
+            
+            return RedirectToAction("Shopping", resultModel);
         }
     }
 
@@ -36,8 +128,8 @@ public class ShoppingController : Controller
             {
                 var line = reader.ReadLine();
                 var values = line.Split(',');
-                var county = values[0];
                 var district = values[1];
+                var county = values[0];
 
                 if (!districtCounties.ContainsKey(district))
                 {
@@ -50,4 +142,5 @@ public class ShoppingController : Controller
 
         return districtCounties;
     }
+
 }
